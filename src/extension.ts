e804c8a51863dfa6be45c48af91ecb8485594b8b
Dmitry Doroshev <r.schweppes@ya.ru>
2017-09-15 09:46:27 +0300

    Add color customizations for themes

--- src/extension.ts
+++ src/extension.ts
@@ -4,12 +4,15 @@ import * as vscode from 'vscode';
 const userConfig = vscode.workspace.getConfiguration();
 let extensionConfig;
 let nightTheme, dayTheme;
+let nightThemeCustomizations, dayThemeCustomizations;
 
 function updateSettings() {
   console.log("updated settings");
   extensionConfig = vscode.workspace.getConfiguration('dayNightThemeSwitcher');
   nightTheme = extensionConfig.nightTheme;
   dayTheme = extensionConfig.dayTheme;
+  nightThemeCustomizations = extensionConfig.nightThemeCustomizations;
+  dayThemeCustomizations = extensionConfig.dayThemeCustomizations;
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -19,11 +22,13 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.commands.registerCommand('extension.switchToNightTheme', () => {
     console.log("to night: " + nightTheme);
     userConfig.update("workbench.colorTheme", nightTheme, true);
+    userConfig.update("workbench.colorCustomizations", nightThemeCustomizations, true);
   });
 
   vscode.commands.registerCommand('extension.switchToDayTheme', () => {
     console.log("to day: " + dayTheme);
     userConfig.update("workbench.colorTheme", dayTheme, true);
+    userConfig.update("workbench.colorCustomizations", dayThemeCustomizations, true);
   });
 
   context.subscriptions.push(disposable);  
